6cf28c6ff8be34a22ba642ad123bac739c8ffcc6
Stefanino <stefanino.exe@gmail.com>
2018-06-09 07:45:58 +0200

    Test

---
+++ bckndepumpe/src/main/java/ep/rs/bckndepumpe/App.java
@@ -0,0 +1,13 @@
+package ep.rs.bckndepumpe;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
